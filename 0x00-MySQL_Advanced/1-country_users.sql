78ff380b43fbbb434bb521d2647b34c2aacaa7c4
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-12 18:24:20 +0100

    updating enum value fixing it up

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL,
 	name VARCHAR(255),
-	country ENUM ('US', 'CO', 'TN') DEFAULT ''
-	-- (default NULL, or '' if NOT NULL)
+	country ENUM ('US', 'CO', 'TN') DEFAULT '',
+	UNIQUE (email),
+	PRIMARY KEY (id),
 );
