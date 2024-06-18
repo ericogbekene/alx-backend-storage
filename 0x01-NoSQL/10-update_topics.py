7e8a5a03a127fd61d7c736fe3439bd3951a570fb
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-18 20:39:06 +0100

    update a record

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,9 +3,9 @@
 update a record
 """
 
-
-def update_topics(mongo_collection, name, topics):
-    """
-    function to update a record
-    """
-    mongo_collection.updateMany({"name": name} {"$set": {"topics": topics}})
+if __name__ == '__main__':
+    def update_topics(mongo_collection, name, topics):
+        """
+        function to update a record
+        """
+        mongo_collection.updateMany({"name": name} {"$set": {"topics": topics}})
