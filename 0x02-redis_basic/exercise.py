e203ba4d4b07b2fb3e9424b4a7649f97d4556465
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-20 02:27:24 +0100

    implementing get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@ module to setup a python redis connection
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional
 
 
 class Cache:
@@ -27,3 +27,24 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[callable]):
+        """
+        method to return utf exuivalent option from cache
+        """
+        if key:
+            value = self._redis.get(key)
+            if fn:
+                return fn(value)
+
+    def get_str(self, key:bytes) -> str:
+        """
+        to return a str
+        """
+        return self.get(key, lambda x: x.decode("utf-8"))
+    
+    def get_int(self, key:bytes) -> int:
+        """
+        this will return and int from bytes
+        """
+        return self.get(key, lambda x: int.from_bytes(x, "big"))
