cadd51cbebe8cc4ea9e56a8e4884395b9f4f1d89
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-22 22:54:15 +0100

    update topics based on name in a collection

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,9 +3,9 @@
 update a record
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__"
     def update_topics(mongo_collection, name, topics):
         """
         function to update a record
         """
-        mongo_collection.updateMany({"name": name}, {"$set": {"topics": topics}})
+        mongo_collection.updateMany({name: name}, {"$set": {topics: topics}})
